adbc0055b46c062978d32e3d2542cdffe5d2e1f6
Samiksha pol <samikshapol1823@gmail.com>
2024-12-10 18:26:41 +0530

    first commit

---
+++ src/test/java/com/tka/ClassroomandAtendancemanagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomandAtendancemanagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
